e3ddd447f37c9b0db8da06080dcf6c0904f6b555
Christian Rodriguez <christian@totallifechanges.com>
2019-10-25 23:06:00 -0400

    first commit

---
+++ Lab_7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+
+            Console.Write("Please enter a valid name: ");
+            userInput = Console.ReadLine();
+            Validation.ValidateNames(userInput);
+            
+            Console.Write("Please enter a valid email: ");
+            userInput = Console.ReadLine();
+            Validation.ValidateEmails(userInput);
+
+            Console.Write("Please enter a valid phone: ");
+            userInput = Console.ReadLine();
+            Validation.ValidatePhone(userInput);
+
+            Console.Write("Please enter a valid date: ");
+            userInput = Console.ReadLine();
+            Validation.ValidateDate(userInput);
+
+            Console.Write("Please enter valid HTML Elements: ");
+            userInput = Console.ReadLine();
+            Validation.ValidateHTML(userInput);
+        }
+    }
+}
---
+++ Lab_7/Validation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lab_7
+{
+    class Validation
+    {
+        public static void ValidateNames(string input)
+        {
+            if(Regex.IsMatch(input, @"(^[A-Z]{1}[A-z]{0,29}$)"))
+            {
+                Console.WriteLine("Name is valid!\n");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, name is not valid!\n");
+            }
+        }
+        public static void ValidateEmails(string input)
+        {
+            if(Regex.IsMatch(input, @"(^[A-z|0-9]{5,30}@{1}[A-z|0-9]{5,10}\.{1}[A-z|0-9]{2,3})"))
+            {
+                Console.WriteLine("Email is valid!\n");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, email is not valid!\n");
+            }
+        }
+        public static void ValidatePhone(string input)
+        {
+            if(Regex.IsMatch(input, @"([0-9]{3}-{1}[0-9]{3}-{1}[0-9]{4})"))
+            {
+                Console.WriteLine("Phone is valid!\n");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, phone is not valid!\n");
+            }
+        }
+        public static void ValidateDate(string input)
+        {
+            if(Regex.IsMatch(input, @"^([012][0-9]|3[01])\/{1}([0][0-9]|1[0-2])\/{1}([0-9]{4})"))
+            {
+                Console.WriteLine("Date is valid!\n");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, date is not valid!\n");
+            }
+        }
+        public static void ValidateHTML(string input)
+        {
+            if (Regex.IsMatch(input, @"^(<{1}[A-z]{1,}>{1})(.{0,})(<{1}\/{1}[A-z]{1,}>{1})"))
+            {
+                Console.WriteLine("HTML is valid!\n");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, HTML is not valid!\n");
+            }
+        }
+    }
+}
